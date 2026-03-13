5e0d868f1b98353ee720f2cb25ed7b3130adc86d
NataliaGomesCarnelos <nataliafacens@gmail.com>
2026-03-12 22:18:27 -0300

    add flags necessarias para selenium em ci

--- src/main/java/framework/driver/DriverFactory.java
+++ src/main/java/framework/driver/DriverFactory.java
@@ -1,10 +1,12 @@
-
 package framework.driver;
 
 import framework.config.ConfigReader;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
+
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
@@ -31,8 +33,22 @@ public class DriverFactory {
                     break;
 
                 default:
+
                     WebDriverManager.chromedriver().setup();
-                    driver = new ChromeDriver();
+
+                    ChromeOptions options = new ChromeOptions();
+
+                    // Flags necessárias para CI (GitHub Actions)
+                    boolean headless = Boolean.parseBoolean(System.getenv("CI"));
+
+                    if (headless) {
+                        options.addArguments("--headless=new");
+                        options.addArguments("--no-sandbox");
+                        options.addArguments("--disable-dev-shm-usage");
+                        options.addArguments("--window-size=1920,1080");
+                    }
+
+                    driver = new ChromeDriver(options);
             }
 
             driver.manage().window().maximize();
